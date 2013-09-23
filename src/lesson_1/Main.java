18e4efe88c74cc65b54549a64c792ca843981b99
nikita <nik.krichko@ya.ru>
2013-09-23 08:55:58 +0300

    working test from ubuntu // NO CHANGES

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -1,12 +1,6 @@
 package lesson_1;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Nikita
- * Date: 04.09.13
- * Time: 11:20
- * To change this template use File | Settings | File Templates.
- */
+// test of VCS from ubuntu
 public class Main {
     public static void main(String[] args) {
 
